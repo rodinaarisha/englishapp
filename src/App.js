a6a44dd41a75db613fab5e3e3477f92bdf20428c
ararodina <ararodina@srt.beeline.ru>
2024-08-25 21:54:29 +0400

    fix change path for  pages

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 import Footer from './components/Footer/Footer.jsx';
 import Header from './components/Header/Header.jsx';
 import './components/Table/Table.css';
-import CardPage from './pages/CardPage.jsx';
-import HomePage from './pages/HomePage.jsx';
-import TablePage from './pages/TablePage.jsx';
+import CardPage from './pages/CardPage/CardPage.jsx';
+import HomePage from './pages/HomePage/HomePage.jsx';
+import TablePage from './pages/TablePage/.jsx';
 
 
 import WordListPage from './pages/WordListPage.jsx';
@@ -21,7 +21,7 @@ const App = () => {
                 {/* Определение маршрутов */}
                 <Routes> {/* Обновлено на Routes */}
                     <Route path="/" element={<HomePage />} /> {/* Главная страница */}
-                    <Route path="/tableList" element={<TablePage />} /> {/* Страница карточек */}
+                    <Route path="/tablelist" element={<TablePage />} /> {/* Страница карточек */}
                     <Route path="/card" element={<CardPage />} /> {/* Страница карточек */}
                     <Route path="/wordcardlist" element={<WordListPage/>} /> {/* Страница карточек */}
                     {/* Добавьте маршрут для страницы WordList, если она будет создана */}
